d559a704227ad8ebf9a54510547019f12406bcc6
Guillaume Denry <guillaume.denry@gmail.com>
2022-03-28 11:32:35 +0200

    fix: psock.js becomes the only entrypoint for both CLI and library usage mode

--- app.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict'
-
-const path = require('path')
-const fs = require('fs')
-const realPath = fs.realpathSync(__dirname)
-const script = path.join(realPath, 'psock.js')
-
-require(script.toString())
