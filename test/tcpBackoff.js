b390da703059b8c585b049ea2b88cb1bbb1455e4
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-07-09 12:36:39 +0200

    make sure that the backoff delay increased

--- test/tcpBackoff.js
+++ test/tcpBackoff.js
@@ -6,22 +6,23 @@ const { ExponentialStrategy } = require('backoff')
 const { expect } = require('chai')
 
 test('tcp backoff', function testTcpBackoff (done) {
-  let retryCount = 0
+  const exponentialStrategy = new ExponentialStrategy({
+    initialDelay: 10,
+    factor: 10 // 10, 100, 1000, 2000...
+  })
   const tcpConnection = TcpConnection({
     address: '127.0.0.1',
     port: 0,
     reconnect: true,
-    backoffStrategy: new ExponentialStrategy({
-      initialDelay: 10,
-      factor: 10 // 10, 100, 1000, 2000...
-    }),
+    backoffStrategy: exponentialStrategy,
     onSocketClose: () => {
-      retryCount++
+      // noop
     }
   })
   setTimeout(() => {
     tcpConnection.end('', 'utf8', () => done())
     // initial, 10, 100
-    expect(retryCount).to.equal(3)
+    const nextBackoffDelay = exponentialStrategy.next()
+    expect(nextBackoffDelay).to.be.gt(100)
   }, 500)
 })
