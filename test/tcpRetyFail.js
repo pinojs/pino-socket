ebca3e9cb57c85f2c3a1adbfd226c5fe1ae3e88a
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-07-27 12:09:00 +0200

    Emit an reconnectFailure event
    When the maximum number of backoffs is reached. Error can be undefined.

---
+++ test/tcpRetyFail.js
@@ -0,0 +1,24 @@
+'use strict'
+/* eslint-env node, mocha */
+
+const { expect } = require('chai')
+const TcpConnection = require('../lib/TcpConnection')
+
+test('tcp retry fail', function testTcpRetryFail (done) {
+  let socketErrorCount = 0
+  const tcpConnection = TcpConnection({
+    address: '127.0.0.1',
+    port: 0,
+    reconnect: true,
+    reconnectTries: 2
+  }
+  )
+  tcpConnection.on('socketError', () => {
+    socketErrorCount++
+  })
+  tcpConnection.on('reconnectFailure', (lastError) => {
+    expect(socketErrorCount).to.eq(3)
+    expect(lastError).to.be.an('error')
+    done()
+  })
+})
