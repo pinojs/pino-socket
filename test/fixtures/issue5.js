5c3689e95a5a99712b8ad69c3adc6526c5980195
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-01-03 17:04:39 +0100

    refactor: simplify issue5.js and update test structure

--- test/fixtures/issue5.js
+++ test/fixtures/issue5.js
@@ -1,14 +1,5 @@
 'use strict'
 
-const int = setInterval(() => {}, 10)
-process.on('message', () => {
-  process._rawDebug('!!! doit')
-  clearInterval(int)
-  replication()
-})
-
-function replication () {
-  const pino = require('pino')()
-  pino.info('hello world')
-  setTimeout(() => { throw new Error('uncaught') }, 20)
-}
+const pino = require('pino')()
+pino.info('hello world')
+setTimeout(() => { throw new Error('uncaught') }, 20)
--- test/utils.js
+++ test/utils.js
@@ -46,8 +46,19 @@ function createSecureTcpListener (msgHandler) {
   })
 }
 
+function withResolvers () {
+  let _resolve
+  let _reject
+  const promise = new Promise((resolve, reject) => {
+    _resolve = resolve
+    _reject = reject
+  })
+  return { promise, resolve: _resolve, reject: _reject }
+}
+
 module.exports = {
   createSecureTcpListener,
   createTcpListener,
-  createUdpListener
+  createUdpListener,
+  withResolvers
 }
